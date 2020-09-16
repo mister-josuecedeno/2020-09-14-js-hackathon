262e6255778d9b3db48d9428c0127db524997bab
josuecedeno <josuecedeno@yahoo.com>
2020-09-16 09:05:26 -0500

    Initial commit

--- src/index copy.js
+++ src/index copy.js
@@ -1,55 +1,53 @@
 const canvas = document.querySelector('#draw');
-const line = document.querySelector('#thickness');
-const buttons = document.querySelectorAll('button');
-const eraser = document.querySelector('#eraser');
-
-let paintColor = '#ffbb00';
-
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = line.value;
+ctx.lineWidth = 100;
+// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation
+// ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 let direction = true;
 
 function draw(e) {
   if (!isDrawing) return;
-  ctx.strokeStyle = paintColor;
+  // console.log(e);
+  // https://mothereffinghsl.com/
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY];
-}
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
 
-// Add Eventlisteners
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
+}
 
 canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
   [lastX, lastY] = [e.offsetX, e.offsetY];
 });
+
 canvas.addEventListener('mousemove', draw);
+
 canvas.addEventListener('mouseup', () => (isDrawing = false));
 canvas.addEventListener('mouseout', () => (isDrawing = false));
-
-// Tools
-
-// Thickness
-thickness.addEventListener('change', () => (ctx.lineWidth = line.value));
-
-// Color
-buttons.forEach((button) => {
-  button.addEventListener(
-    'click',
-    (e) => (paintColor = e.target.dataset.color)
-  );
-});
-
-// Eraser
-eraser.addEventListener('click', (e) => (paintColor = e.target.dataset.color));
